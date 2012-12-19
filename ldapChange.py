3b90d52a816ba420329c1864ae4d398349e6ed49
mobjack <ericparker4@live.com>
2012-12-18 16:10:04 -0800

    still working out basic functions

--- ldapChange.py
+++ ldapChange.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python
+#
+# This script will parse the ldap change logs.  It will need to 
+# keep track of the logs already processed by change ID.  It will 
+# then need to send a cef message into syslog.
 
+# Imports
+
+
+# Globals
+logDir = '/Users/eparker/scripts/git/ldap-logs'
+logFile = logDir + '/auditlog.ldif'
+logDb = logDir + '/change.db'
+
+
+def main():
+    prevId = [] 
+    # Create a tuple of all previous logs
+    for line in open(logDb):
+        line = line.strip()
+        prevId.append(line)
+
+    print prevId
+    
+    
+
+
+if __name__ == '__main__':
+    main()
